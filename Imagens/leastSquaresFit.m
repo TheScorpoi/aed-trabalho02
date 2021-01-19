874b93cd6961ca41336f4cb14adc675fbfe33e06
TheScorpoi <66091656+TheScorpoi@users.noreply.github.com>
2021-01-19 19:03:18 +0000

    least squares

--- Imagens/leastSquaresFit.m
+++ Imagens/leastSquaresFit.m
@@ -12,24 +12,24 @@ strs = ["Bubble Sort" , "Shaker Sort", "Insertion Sort" , "Shell Sort" , "Quick 
 for o = 1:(length(idx)-1)
     i = idx(o);
     seguinte = idx(o + 1);
-    x = avg_time(i:seguinte,1); % extract the first column
-    y = avg_time(i:seguinte , 2);% extract the second column
+    x = avg_time(i:seguinte,1);     % extract the first column
+    y = avg_time(i:seguinte , 2);   % extract the second column
     
     if(o < 4 || o > 7)
-        X = [ x.^2, x, 0*x+1 ];% build the X matrix
+        X = [ x.^2, x, 0*x+1 ];     % build the X matrix
     else
         X = [ x.*log(x), x, 0*x+1 ];% build the X matrix
     end
     
-    w = pinv(X)*y;% optimal solution (could also be written as w = X \ y;)
+    w = pinv(X)*y;                  % optimal solution (could also be written as w = X \ y;)
     e = y-X*w;
     format long;
     norm(e)
     figure;
-    plot(x,y,'.r',x,X*w,'og');% plot the original data and its best least squares approximation
+    plot(x,y,'.b',x,X*w,'or');      % plot the original data and its best least squares approximation
     title(strs(o));
     xlabel("n");
     ylabel("Tempo(s)");
-    legend('Tempos mÃ©dios','X*w')
+    legend('Tempos médios','X*w', 'Location', 'northwest', 'FontSize', 14)
   
 end
